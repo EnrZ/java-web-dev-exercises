08ea8a2c139291fab0523d8b3be7607b88dd7347
THEnter <ez_25_25@yahoo.com>
2022-04-14 19:50:55 -0500

    2 bonus parts done

--- src/studios/CountingChars.java
+++ src/studios/CountingChars.java
@@ -7,7 +7,14 @@ public class CountingChars {
     public static void main(String[] args) {
         HashMap<Character, Integer> charCounting = new HashMap<>();
         String sent = "Counting the characters in this.";
-        char[] charactersInString = sent.toLowerCase().toCharArray();
+        String[] stringArray = sent.split("\\W+");
+        String resultNonAlph = new String();
+        for(int i = 0; i < stringArray.length;i++){
+            resultNonAlph = resultNonAlph + stringArray[i];
+        }
+       // char charactersInString = new char[resultNonAlph.length];
+
+        char[] charactersInString = resultNonAlph.toLowerCase().toCharArray();
         for(int i = 0; i < charactersInString.length; i++){
             if(!charCounting.containsKey(charactersInString[i])) {
                 charCounting.put(charactersInString[i], 1);
@@ -16,7 +23,7 @@ public class CountingChars {
                     charCounting.put(charactersInString[i], charCounting.get(charactersInString[i]).intValue() + 1);
                 }
             }
-        System.out.println(charCounting);
+
         for(Character key: charCounting.keySet()){
             System.out.println(key + ": " + charCounting.get(key));
         }
