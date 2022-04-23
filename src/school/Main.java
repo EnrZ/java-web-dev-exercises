e39ecc2b6cb8a93972a785464c19b8eba14f9a31
THEnter <ez_25_25@yahoo.com>
2022-04-22 23:12:54 -0500

    custom equals works

--- src/school/Main.java
+++ src/school/Main.java
@@ -10,6 +10,14 @@ public class Main {
         c1.studentArrayList.add(new Student("EZ",1,50,4.0));
         c1.studentArrayList.add(new Student("AJ",1,20,4.0));
 
+        Course c2 = new Course();
+
+        c2.studentArrayList.add(new Student("EZ",1,50,4.0));
+        c2.studentArrayList.add(new Student("AJ",1,20,4.0));
+
+        //IntelliJ generated equals function works. If course2 is same as course1, it prints true. Otherwise false.
+        System.out.println(c2.equals(c1));
+
         c1.studentArrayList.get(0).addGrade(3,3);
 
         System.out.println(c1.studentArrayList.get(0).getGpa());
